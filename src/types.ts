d7003624c95589a717ef3d84daad8896387ddac7
Varis Nijat <vnijat93@gmail.com>
2024-11-19 22:45:52 -0500

    DEV Add new type

--- src/types.ts
+++ src/types.ts
@@ -6,6 +6,12 @@ export interface Receipt {
     total: string;
 }
 
+export interface ProcessedReceipt {
+    id: string;
+    receipt: Receipt;
+    points: number;
+}
+
 export interface Item {
     shortDescription: string;
     price: string;
